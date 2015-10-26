e30fa57062b4195bf95f6777607957096a5c5d17
Zorox3 <timogoe@gmail.com>
2015-10-26 13:37:42 +0100

    GUI selection, WIP

--- NJDEngine/src/gui/GUI.java
+++ NJDEngine/src/gui/GUI.java
@@ -2,19 +2,24 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import management.NJDE;
 import display.Display;
+import events.Tickable;
+import renderer.FormRenderer;
 import renderer.Renderable;
 import renderer.TextRenderer;
 
-public class GUI implements Renderable {
+public class GUI implements Renderable, Tickable {
 
 	private String headline;
 	private Color textColor = Color.WHITE;
 	private int fontsize = 0;
 	private int x, y;
+	private int selected = 0;
 
 	private List<GUI_Elements> elements = new ArrayList<>();
 
@@ -26,6 +31,9 @@ public class GUI implements Renderable {
 		this.textColor = textColor;
 		this.x = x;
 		this.y = y;
+
+		
+		
 	}
 
 	@Override
@@ -35,13 +43,24 @@ public class GUI implements Renderable {
 
 		int elementOffset = (int) (fontsize * 6);
 
+		int counter = 0;
+		
+		
+		
 		for (GUI_Elements e : elements) {
-
+			
 			e.setX(x);
 			e.setY(elementOffset);
 
+			if(counter == selected){
+				FormRenderer.filledOval(x - 30, elementOffset - fontsize / 2, fontsize/2, fontsize/2, textColor);
+			}
+			
+			
 			e.render();
 			elementOffset += fontsize * 1.5;
+			
+			counter++;
 		}
 	}
 
@@ -67,6 +86,34 @@ public class GUI implements Renderable {
 
 	public List<GUI_Elements> getElements() {
 		return elements;
+	}
+
+	@Override
+	public void tick() {
+
+		if (NJDE.key.isKeyDown(KeyEvent.VK_UP)
+				|| NJDE.key.isKeyDown(KeyEvent.VK_W)) {
+			selected--;
+			NJDE.print("UP");
+			if (selected < 0) {
+				selected = elements.size();
+			}
+		} else if (NJDE.key.isKeyDown(KeyEvent.VK_DOWN)
+				|| NJDE.key.isKeyDown(KeyEvent.VK_S)) {
+			selected++;
+			if (selected > elements.size() - 1) {
+				selected = 0;
+			}
+		}
+		
+	}
+
+	public int getSelected() {
+		return selected;
+	}
+
+	public void setSelected(int selected) {
+		this.selected = selected;
 	}
 
 }
